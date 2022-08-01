87ba151488d289496a127b3981247be963054ea0
RoeeShoshani <Roee Shoshani>
2022-08-02 00:23:24 +0300

    now using the set_timeout crate for timeouts

--- shithead_game_server/src/util.rs
+++ shithead_game_server/src/util.rs
@@ -1,3 +1,9 @@
+use set_timeout::TimeoutScheduler;
+
+lazy_static::lazy_static! {
+    pub static ref TIMEOUT_SCHEDULER: TimeoutScheduler = TimeoutScheduler::new(None);
+}
+
 #[macro_export]
 macro_rules! some_or_return {
     ($e:expr) => {
